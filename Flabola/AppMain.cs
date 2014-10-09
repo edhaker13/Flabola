13f97edf1baf45461834627811f072ecc086cc2e
Luis Checa <c008101c@student.staffs..ac.uk>
2014-10-09 03:04:15 +0100

    Add UI Toolkit, Score, HitCollision

--- Flabola/AppMain.cs
+++ Flabola/AppMain.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 
+using Sce.PlayStation.HighLevel.UI;
 using Sce.PlayStation.Core;
 using Sce.PlayStation.Core.Environment;
 using Sce.PlayStation.Core.Graphics;
@@ -11,38 +12,77 @@ namespace Flabola
 	public class AppMain
 	{
 		private static GraphicsContext graphics;
+		private static int screenWidth, screenHeight;
+		private static int playerScore;
+		private static bool quit;
 		
-		public static void Main (string[] args)
+		public static void Main(string[] args)
 		{
 			Initialize ();
 
-			while (true) {
+			while (!quit)
+			{
 				SystemEvents.CheckEvents ();
 				Update ();
 				Render ();
 			}
 		}
-
-		public static void Initialize ()
+		
+		public static int PlayerScore
 		{
-			// Set up the graphics system
-			graphics = new GraphicsContext ();
+			get{ return playerScore; }
+		}
+		
+		public static bool Quit
+		{
+			get{ return quit; }
+			set{ quit = value; }
 		}
 
-		public static void Update ()
+		public static void Initialize()
+		{
+			// Initial player score
+			playerScore = 0;
+			// Set up the graphics system
+			graphics = new GraphicsContext ();
+			// Initialize UI system
+			UISystem.Initialize(graphics);
+			// Retrieve screen width and height
+			screenWidth = UISystem.FramebufferWidth;
+			screenHeight = UISystem.FramebufferHeight;
+		}
+
+		public static void Update()
 		{
 			// Query gamepad for current state
 			var gamePadData = GamePad.GetData (0);
+			// Query touch for current state
+            List<TouchData> touchDataList = Touch.GetData (0);
+
+            // Update UI Toolkit
+            UISystem.Update(touchDataList);
 		}
 
-		public static void Render ()
+		public static void Render()
 		{
 			// Clear the screen
 			graphics.SetClearColor (0.0f, 0.0f, 0.0f, 0.0f);
 			graphics.Clear ();
-
+			// Render the UI screen
+			UISystem.Render();
 			// Present the screen
 			graphics.SwapBuffers ();
 		}
+		
+	    private static bool InsideRect(float pixelX, float pixelY, Rectangle hitTestArea)
+	    {
+			
+	        if (hitTestArea.X <= pixelX && hitTestArea.X + hitTestArea.Width >= pixelX &&
+	            hitTestArea.Y <= pixelY && hitTestArea.Y + hitTestArea.Height >= pixelY) {
+	            return true;
+	        }
+	
+	        return false;
+	    }
 	}
 }
