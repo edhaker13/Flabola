765a131cc60215e7ce0fc912fd624e818b425273
Luis Checa <c008101c@student.staffs.ac.uk>
2014-10-22 10:56:13 +0100

    [+] Replaced pipes with zombies, animation

--- Obstacle.cs
+++ Obstacle.cs
@@ -10,38 +10,34 @@ namespace Flabola
 {
 	public class Obstacle
 	{
-		const float kGap = 200.0f;
+		const float kGap = 300.0f;
 		
 		// Private variables.
 		private SpriteTile[] sprites;
-		private TextureInfo	textureInfoTop;
-		private TextureInfo	textureInfoBottom;
+		private TextureInfo	textureInfo;
 		private Bounds2 pipeGap;
 		private bool isGapOccupied;
 		private float width;
 		private float height;
 		
-		// Accessors.
-		//public SpriteUV SpriteTop 	 { get{return sprites[0];} }
-		//public SpriteUV SpriteBottom { get{return sprites[1];} }
-		
 		// Public functions.
 		public Obstacle(float startX, Scene scene)
 		{
-			textureInfoTop = new TextureInfo("/Application/assets/toppipe.png");
-			textureInfoBottom = new TextureInfo("/Application/assets/bottompipe.png");
+			textureInfo = new TextureInfo(new Texture2D("/Application/assets/zombie_pipes.png", false), new Vector2i(4,8));
 			
 			sprites = new SpriteTile[2];
 			
 			// Top
-			sprites[0] = new SpriteTile(textureInfoTop);	
-			sprites[0].Quad.S = textureInfoTop.TextureSizef;
+			sprites[0] = new SpriteTile(textureInfo);	
+			sprites[0].Quad.S = textureInfo.TileSizeInPixelsf;
+			sprites[0].TileIndex2D = new Vector2i(0, (int)RandomPosition(0f, 4f));
 			// Add to the current scene.
 			scene.AddChild (sprites[0]);
 			
 			// Bottom
-			sprites[1] = new SpriteTile(textureInfoBottom);	
-			sprites[1].Quad.S = textureInfoBottom.TextureSizef;		
+			sprites[1] = new SpriteTile(textureInfo);	
+			sprites[1].Quad.S = textureInfo.TileSizeInPixelsf;
+			sprites[1].TileIndex2D = new Vector2i(0, (int)RandomPosition(0f, 4f));
 			// Add to the current scene.
 			scene.AddChild (sprites[1]);
 			
@@ -51,21 +47,32 @@ namespace Flabola
 			height = b.Point11.Y;
 			
 			// Position pipes.
-			sprites[0].Position = new Vector2(startX, AppMain.ScreenHeight * RandomPosition());
-			sprites[1].Position = new Vector2(startX, sprites[0].Position.Y - height - kGap);
+			sprites[0].Position = new Vector2(startX, AppMain.ScreenHeight * RandomPosition(0.5f, 1.0f - height/AppMain.ScreenHeight/2));
+			sprites[1].Position = new Vector2(startX, AppMain.ScreenHeight * RandomPosition(-height/AppMain.ScreenHeight/2, 0.4f));
 			// Position gap rect
 			pipeGap = new Bounds2(new Vector2(sprites[1].Position.X, sprites[1].Position.Y + height),
 			                      new Vector2(sprites[0].Position.X + width, sprites[0].Position.Y));
 			isGapOccupied = false;
+			
+			sprites[0].Schedule(Update);
+			// Animations
+			sprites[0].ScheduleInterval((dt) => {
+				int tileIndex = sprites[0].TileIndex2D.X < 4 ? sprites[0].TileIndex2D.X + 1: 0;
+				sprites[0].TileIndex2D = new Vector2i(sprites[0].TileIndex2D.Y, tileIndex);
+			}, 0.25f);
+			sprites[1].Schedule(Update);
+			sprites[0].ScheduleInterval((dt) => {
+				int tileIndex = sprites[1].TileIndex2D.X < 4 ? sprites[1].TileIndex2D.X + 1: 0;
+				sprites[1].TileIndex2D = new Vector2i(sprites[1].TileIndex2D.Y, tileIndex);
+			}, 0.25f);
 		}
 		
 		public void Dispose()
 		{
-			textureInfoTop.Dispose();
-			textureInfoBottom.Dispose();
+			textureInfo.Dispose();
 		}
 		
-		public void Update(float deltaTime)
+		public void Update(float dt)
 		{			
 			sprites[0].Position = new Vector2(sprites[0].Position.X - 3, sprites[0].Position.Y);
 			sprites[1].Position = new Vector2(sprites[1].Position.X - 3, sprites[1].Position.Y);
@@ -74,20 +81,19 @@ namespace Flabola
 			// If off the left of the viewport, loop them around.
 			if(sprites[0].Position.X < -width)
 			{
-				sprites[0].Position = new Vector2(AppMain.ScreenWidth, AppMain.ScreenHeight * RandomPosition());
-				sprites[1].Position = new Vector2(AppMain.ScreenWidth, sprites[0].Position.Y - height - kGap);
+				sprites[0].Position = new Vector2(AppMain.ScreenWidth, AppMain.ScreenHeight * RandomPosition(0.5f, 1.0f - height/AppMain.ScreenHeight/2));
+				sprites[1].Position = new Vector2(AppMain.ScreenWidth, AppMain.ScreenHeight * RandomPosition(-height/AppMain.ScreenHeight/2, 0.5f));
 				pipeGap.Min = new Vector2(sprites[1].Position.X, sprites[1].Position.Y + height);
 			}		
 		}
 		
-		private float RandomPosition()
+		private float RandomPosition(float min=0.5f, float max=1.0f)
 		{
 			Random rand = new Random();
-			float randomPosition = (float)rand.NextDouble ();
-			randomPosition += 0.45f;
+			double range = max - min;
+			double sample = rand.NextDouble();
 			
-			if(randomPosition > 1.0f)
-				randomPosition = 0.9f;
+			float randomPosition = (float)(sample * range) + min;
 		
 			return randomPosition;
 		}
