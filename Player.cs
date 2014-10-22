50bc96fc0d7c9edd3855cfd11fd817b59bdda19d
Luis Checa <c008101c@student.staffs.ac.uk>
2014-10-22 11:02:10 +0100

    [+] Added code for death animation

--- Player.cs
+++ Player.cs
@@ -28,7 +28,7 @@ namespace Flabola
 		// Public functions.
 		public Player(Scene scene)
 		{
-			textureInfo = new TextureInfo(new Texture2D("/Application/assets/player-spritemap.png", false), new Vector2i(9,1));
+			textureInfo = new TextureInfo(new Texture2D("/Application/assets/player-spritemap.png", false), new Vector2i(13,1));
 			sprite = new SpriteTile(textureInfo);
 			sprite.Position = new Vector2(AppMain.ScreenWidth * .2f, AppMain.ScreenHeight * .5f);
 			sprite.Quad.S = textureInfo.TileSizeInPixelsf;
@@ -37,54 +37,9 @@ namespace Flabola
 			isAlive = true;
 			moveState = MoveStatus.Disabled;
 			rotateAngle = .0f;
-			float maxAngle = Math.Deg2Rad(15f);
-			float minAngle = -maxAngle;
 			
-			// Create update fucntion
-			sprite.Schedule( (dt) => {
-				// If tap on screen, tell player
-				if(Touch.GetData(0).Count > 0)
-				{
-					this.Tapped();
-				}
-				
-				// Check collision with floor
-				Vector2 pos = sprite.Position;
-				if(pos.Y <= sprite.Scale.Y / 2)
-				{
-					IsAlive = false;
-					return;
-				}
-				
-				// Adjust jump direction and angle
-				switch(moveState)
-				{
-				case MoveStatus.Up:
-					if((pos.Y - yPositionBeforePush) < maxPushAmount)
-					{
-						sprite.Position = new Vector2(pos.X, pos.Y + pushAmount);
-						rotateAngle += dt;
-					}
-					else
-					{
-						moveState = MoveStatus.Down;
-					}
-					break;
-				case MoveStatus.Down:
-					sprite.Position = new Vector2(pos.X, pos.Y - pushAmount);
-					rotateAngle -= dt;
-					break;
-				}
-				
-				// Clip angle bounds
-				if(rotateAngle > maxAngle)
-					rotateAngle -= dt;
-				else if (rotateAngle < minAngle)
-					rotateAngle += dt;
-				
-				// Do the actual rotation
-				sprite.Rotation = Vector2.Rotation(rotateAngle);	
-			});
+			// Attach update fucntion to scheduler
+			sprite.Schedule(Update);
 			
 			// Create animation function
 			sprite.ScheduleInterval( (dt) => {
@@ -93,15 +48,72 @@ namespace Flabola
 					int tileIndex = sprite.TileIndex1D < 8 ? sprite.TileIndex1D + 1 : 1;
 					sprite.TileIndex1D = tileIndex;
 				}
-			}, 0.3f);
+			}, 0.2f);
+			sprite.ScheduleInterval((dt) => {
+				if(!IsAlive)
+				{
+					int tileIndex = sprite.TileIndex1D < 12 ? sprite.TileIndex1D + 1 : 12;
+					sprite.TileIndex1D = tileIndex;
+				}
+			}, 0.16f);
 			
 			// Add to the current scene.
 			scene.AddChild(sprite);
 		}
 		
-		public void Dispose()
+		public void Update(float dt)
 		{
-			textureInfo.Dispose();
+			// If tap on screen, tell player
+			if(Touch.GetData(0).Count > 0)
+			{
+				this.Tapped();
+			}
+			
+			// Check collision with floor
+			Vector2 pos = sprite.Position;
+			if(pos.Y <= sprite.Scale.Y / 2)
+			{
+				IsAlive = false;
+				return;
+			}
+			
+			// Disable jump on end of death animation
+			if (IsDead)
+			{
+				moveState = MoveStatus.Disabled;
+			}
+			
+			// Jump variables
+			float maxAngle = Math.Deg2Rad(15f);
+			float minAngle = -maxAngle;
+			// Adjust jump direction and angle
+			switch(moveState)
+			{
+			case MoveStatus.Up:
+				if((pos.Y - yPositionBeforePush) < maxPushAmount)
+				{
+					sprite.Position = new Vector2(pos.X, pos.Y + pushAmount);
+					rotateAngle += dt;
+				}
+				else
+				{
+					moveState = MoveStatus.Down;
+				}
+				break;
+			case MoveStatus.Down:
+				sprite.Position = new Vector2(pos.X, pos.Y - pushAmount);
+				rotateAngle -= dt;
+				break;
+			}
+			
+			// Clip angle bounds
+			if(rotateAngle > maxAngle)
+				rotateAngle -= dt;
+			else if (rotateAngle < minAngle)
+				rotateAngle += dt;
+			
+			// Apply the rotation
+			sprite.Rotation = Vector2.Rotation(rotateAngle);
 		}
 		
 		public bool IsAlive 
@@ -110,17 +122,28 @@ namespace Flabola
 			set{ isAlive = value; }
 		}
 		
-		public void Tapped()
+		public bool IsDead
 		{
-			if(moveState != MoveStatus.Up) {
-				moveState = MoveStatus.Up;
-				yPositionBeforePush = sprite.Position.Y;
-			}
+			get{ return !isAlive && sprite.TileIndex1D == 12; }
 		}
 		
 		public SpriteTile Sprite
 		{
 			get { return sprite; }
+		}
+		
+		public void Tapped()
+		{
+			if(moveState != MoveStatus.Up)
+			{
+				moveState = MoveStatus.Up;
+				yPositionBeforePush = sprite.Position.Y;
+			}
+		}
+		
+		public void Dispose()
+		{
+			textureInfo.Dispose();
 		}
 	}
 }
